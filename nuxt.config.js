105cfdbfc41344a8504d5decf722e76925e2e599
ali deylamy <96489841+DEYLAMY@users.noreply.github.com>
2022-08-16 17:29:49 +0430

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,7 +81,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: 'http://localhost:3000'
-    baseURL: 'https://imaje.herokuapp.com'
+    baseURL: 'https://ali-ali.herokuapp.com'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
