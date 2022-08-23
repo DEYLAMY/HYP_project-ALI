43d0e4c97d51fb33f8db8738ff73322b4894ed4d
ali deylamy <96489841+DEYLAMY@users.noreply.github.com>
2022-08-23 23:09:27 +0430

    Update TheHeader.vue

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -163,7 +163,7 @@ export default {
         },
         {
           name: "Events",
-          path: "/all-Festivals",
+          path: "/all-pois",
         },
         {
           name: "Places",
